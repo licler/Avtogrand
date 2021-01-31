cba080920cfcd505e695b561070bbd143486c525
licler <ute3@yandex.ru>
2021-01-31 20:18:30 +0300

    tarif start

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,7 @@ $(function(){
             $(this).addClass('active');
             $('#'+id).addClass('active-tab').fadeIn();
             return false;
-        });
+    });
 
         $('.tab-box__items').slick({
             infinite: true,
@@ -23,9 +23,18 @@ $(function(){
             slidesToScroll: 4,
             arrows: true,
             prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/advantages/previous.png" alt=""></button>',
-            nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/advantages/next.png" alt=""></button>'
+            nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/advantages/next_1.png" alt=""></button>'
           });
-          
+
+          $('.tarif__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.tarif__tabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.tarif__tabs .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+    });
+         
 	
 	
 });
