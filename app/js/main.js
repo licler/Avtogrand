00a5a8e9907de0043b4af3232770898bccb2e78d
licler <ute3@yandex.ru>
2021-02-09 21:10:13 +0300

    animation start

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,6 @@
 $(function(){
+  
+  
     $('.reviews-page__tabs .tab').on('click', function(event) {
         var id = $(this).attr('data-id');
             $('.reviews-page__tabs').find('.tab-item').removeClass('active-tab').hide();
@@ -34,7 +36,13 @@ $(function(){
             $('#'+id).addClass('active-tab').fadeIn();
             return false;
     });
-    new WOW().init();
+    
+    
+    
+      new WOW().init();
+   
+    
+   
          
 	
 	
--- gulpfile.js
+++ gulpfile.js
@@ -83,7 +83,7 @@ function css() {
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/magnific-popup/dist/magnific-popup.css',
-        'node_modules/animate.css/animate.min.css',
+        'node_modules/animate.css/animate.css',
         'app/scss/style.scss'
       ])  
       .pipe(scss({outputStyle: 'compressed'}))
@@ -119,7 +119,7 @@ function js() {
         'node_modules/jquery/dist/jquery.js',
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/wowjs/dist/wow.min.js',
+        'node_modules/wow.js/dist/wow.js',
         'app/js/main.js'  
     
     ])
